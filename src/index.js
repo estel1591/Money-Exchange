bdceea7a521e5ea9d71ca6d1f1a69298b6792e15
estel1591 <estel1591@mail.ru>
2018-09-27 21:06:10 +0300

    update index.js (correct function makeExchange)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let countCoints = {};
+    while (currency) {
+        if (currency > 10000){
+            countCoints.error = "You are rich, my friend! We don't have so much coins for exchange";
+            break;
+        } else if (currency > 49 && currency < 10001) {
+            countCoints["H"] = parseInt(currency/50, 10);
+            currency = currency - countCoints["H"]*50;
+        } else if (currency > 24 && currency < 50) {
+            countCoints["Q"] = parseInt(currency/25, 10);
+            currency = currency - countCoints["Q"]*25;
+        } else if (currency > 9 && currency < 25) {
+            countCoints["D"] = parseInt(currency/10, 10);
+            currency = currency - countCoints["D"]*10;
+        } else if (currency > 4 && currency < 10) {
+            countCoints["N"] =  parseInt(currency/5, 5);
+            currency = currency - countCoints["N"]*5;
+        }else if (currency > 0 && currency < 5) {
+            countCoints["P"] = currency;
+            currency = currency - countCoints["P"];
+        } else {
+            break;
+        }
+    }
+
+    return countCoints;
 }
